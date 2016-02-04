c9591f4ac10f9fb14696278f12d5f36e6dd0b667
Mohamed Boudra <boudra.moha@gmail.com>
2016-02-04 23:57:55 +0100

    refactor event dispatcher

--- src/Event/EventDispatcher.hpp
+++ src/Event/EventDispatcher.hpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <functional>
 #include <vector>
+#include <map>
 
 #include "TypeId/Any.hpp"
 #include "Event/Event.hpp"
@@ -11,58 +12,45 @@
 
 namespace dm {
 
-typedef std::function<void(const Event&)> ListenerFunc;
-
-using ListenerVector = std::vector<std::function<void(const Event&)>>;
+using HandlerFunction = std::function<void(const Event&)>;
+using ListenerVector = std::vector<HandlerFunction>;
 
 class EventDispatcher {
    public:
-    EventDispatcher() { m_listeners.resize(EventType::NUM_EVENTS); }
+    EventDispatcher() {}
     ~EventDispatcher() {}
 
     template <class HandlerType, class EventValueType = Event>
     size_t subscribe(HandlerType* handler,
                      void (HandlerType::*func)(const EventValueType&),
                      EventType type) {
+
         using realfunc = void (HandlerType::*)(const Event&);
 
         realfunc real = reinterpret_cast<realfunc>(func);
 
-        if (m_listeners.size() <= type || m_handlers.size() <= type) {
-            m_listeners.resize(type + 1);
-            m_handlers.resize(type + 1);
-        }
+        auto r = _listeners.insert(std::make_pair(type, ListenerVector()));
 
-        m_listeners[type].push_back(
-            std::bind(real, (handler), std::placeholders::_1));
+        ListenerVector& handlers = r.first->second;
 
-        /* TODO FIX COUNTER */
-        size_t subscribe_id = m_listeners[type].size() - 1;
+        handlers.push_back(std::bind(real, (handler), std::placeholders::_1));
 
-        m_handlers[type].push_back(
-            std::make_pair(reinterpret_cast<uintptr_t>(handler), subscribe_id));
-
-        return subscribe_id;
+        return handlers.size() - 1;
     }
 
     void unsubscribe(EventType type, unsigned int listenerId) {
-        /*
-        auto& listeners =
-        m_listeners[e.Type].cast<std::vector<std::function<void
-        (const EventValueType&)>>>();
-        listeners.erase(listeners.begin() + listenerId);çç*/
     }
 
     template <class EventValueType>
     void send_event(const EventValueType& e) {
-        for (auto& handler : m_listeners[e.type]) {
+        for (auto& handler : _listeners[e.type]) {
             handler(e);
         }
     }
 
    private:
-    std::vector<ListenerVector> m_listeners;
-    std::vector<std::vector<std::pair<uintptr_t, size_t>>> m_handlers;
+    std::map<EventType, ListenerVector> _listeners;
+
 };
 }
 
